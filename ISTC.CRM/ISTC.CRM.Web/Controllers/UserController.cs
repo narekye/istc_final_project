95ca6068123c0e1f950f10dae3d4989007bdc8fb
Narek Yegoryan <Yegoryan.narek@gmail.com>
2019-04-30 19:04:11 +0400

    Working api version

---
+++ ISTC.CRM/ISTC.CRM.Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ISTC.CRM.Web.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok(new[] { "value1", "value2" });
+        }
+    }
+}
--- ISTC.CRM/ISTC.CRM.Web/Startup.cs
+++ ISTC.CRM/ISTC.CRM.Web/Startup.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace ISTC.CRM.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", o =>
+                {
+                    o.AllowAnyOrigin();
+                    o.AllowCredentials();
+                    o.AllowAnyMethod();
+                    o.AllowAnyHeader();
+                });
+            });
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAll");
+            app.UseWelcomePage("/");
+            app.UseMvc();
         }
     }
 }
