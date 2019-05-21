509bd75a93ce144b42b9c21afe8dd28fbe97740b
narekye <Yegoryan.narek@gmail.com>
2019-05-21 19:01:46 +0400

    Update context

---
+++ ISTC.CRM/ISTC.CRM.DAL/Models/OctoContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ISTC.CRM.DAL.Models
+{
+    public partial class OctoContext : DbContext
+    {
+        public OctoContext()
+        {
+        }
+
+        public OctoContext(DbContextOptions<OctoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=visual.istclabz.com;Integrated Security=false;Initial Catalog=istc_crm;User id=istc_student;Password=Qwe@1234;Encrypt=True;persist security info=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+        }
+    }
+}
